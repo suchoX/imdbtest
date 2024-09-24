121d06651216850a0b7ae55ddc1f7fa5306dcc65
Suchandrim Sarkar <suchandrim.sarkar@gmail.com>
2024-09-24 11:26:39 +0530

    Added Basic Fetch api call

---
+++ src/api/Fetch.ts
@@ -0,0 +1,15 @@
+import {TrendingPageData} from './ResponseData';
+
+const API_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmE2ODYwMWExNWIwNDcwMDFiZDA3NjE2NTIzMDQ5ZiIsIm5iZiI6MTcyNzE1NTg3Ni42NzEzNSwic3ViIjoiNWE3MWEyZGVjM2EzNjg2MDk3MDA1Njk1Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.6BsWd6HaVkSVXUmhWJybvNx5qm9d-sD0l9XthKaxaxI';
+const TRENDING_API = 'https://api.themoviedb.org/3/trending/movie/day';
+
+export const fetchTrendingMovies = async (): Promise<TrendingPageData> => {
+  const response = await fetch(TRENDING_API, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      accept: 'application/json',
+    },
+  });
+  return response.json() as unknown as TrendingPageData;
+};
--- src/ui/screens/HomeScreen.tsx
+++ src/ui/screens/HomeScreen.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {fetchTrendingMovies} from '../../api/Fetch';
 
 const HomeScreen = ({navigation}) => {
+  useEffect(() => {
+    fetchTrendingMovies()
+      .then(data => {
+        console.log(JSON.stringify(data, null, 2));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
   return (
     <SafeAreaView>
       <View>
